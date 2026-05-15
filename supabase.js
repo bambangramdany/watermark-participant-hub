d508e19d98a35db671abce01a9105b4a34f5bc90
bambangramdany <137467600+bambangramdany@users.noreply.github.com>
2026-05-15 15:00:00 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://https://supabase.com/dashboard/project/cphiqzvthsbrycujanqg";
+const SUPABASE_URL = "https://cphiqzvthsbrycujanqg.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwaGlxenZ0aHNicnljdWphbnFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MjQwODgsImV4cCI6MjA5NDQwMDA4OH0.x5hqdID7FiWD4h4-wMoFTdbnI0qW59ovjFGHwCLN4Vo";
 
